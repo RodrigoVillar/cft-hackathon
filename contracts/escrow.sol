944c3130da2d0408575527ab9eff340f6700b86b
Rodrigo Villar <rodrigojvillarg@gmail.com>
2022-04-30 08:22:53 -0400

    begin revision

--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -1,85 +1,133 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.0;
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+// //import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 
-contract NFTescrow {
-    enum Ready_One {
-        ALICE,
-        BOB,
-        NULL
+// contract NFTescrow {
+//     enum Ready_One {
+//         ALICE,
+//         BOB,
+//         NULL
+//     }
+
+//     enum State {
+//         INITIATED,
+//         READY_ONE,
+//         READY_SWAP,
+//         COMPLETE
+//     }
+
+//     struct Escrow {
+//         address alice;
+//         address bob;
+//         address aliceNFT;
+//         address bobNFT;
+//         string aliceSubnet;
+//         string bobSubnet;
+//         State currentState;
+//         Ready_One readyOne;
+//     }
+
+//     uint256 public nextEscrow;
+
+//     mapping(uint256 => Escrow) public escrows;
+
+//     address relayer;
+
+//     modifier _isRelayer(address _relayer) {
+//         require(_relayer == relayer, "Not Relayer!");
+//         _;
+//     }
+
+//     function initiate(
+//         address _alice,
+//         address _bob,
+//         address _aliceNFT,
+//         address _bobNFT,
+//         string memory _alicesubnet,
+//         string memory _bobSubnet
+//     ) external _isRelayer(msg.sender) {
+//         escrows[nextEscrow] = Escrow(
+//             _alice,
+//             _bob,
+//             _aliceNFT,
+//             _bobNFT,
+//             _alicesubnet,
+//             _bobSubnet,
+//             State.INITIATED,
+//             Ready_One.NULL
+//         );
+//         nextEscrow++;
+//     }
+
+//     function ready(address _person, uint256 _escrowID)
+//         external
+//         _isRelayer(msg.sender)
+//     {
+//         Escrow storage escrow = escrows[_escrowID];
+//         if (escrow.currentState == State.INITIATED) {
+//             escrow.currentState = State.READY_ONE;
+//             if (_person == escrow.alice) {
+//                 escrow.readyOne = Ready_One.ALICE;
+//             } else if (_person == escrow.bob) {
+//                 escrow.readyOne = Ready_One.BOB;
+//             }
+//         } else if (escrow.currentState == State.READY_ONE) {
+//             escrow.currentState = State.READY_SWAP;
+//         }
+//     }
+
+//     function swapDone(uint256 _escrowID) external _isRelayer(msg.sender) {
+//         Escrow storage escrow = escrows[_escrowID];
+//         require(escrow.currentState == State.READY_SWAP);
+//         escrow.currentState = State.COMPLETE;
+//     }
+// }
+
+/*
+Smart contract to be stored on NFTEScrow Subnet
+*/
+contract Escrow {
+
+    // Event to be emitted when new escrow tx is initiated
+    event NewEscrow(bytes32 indexed escrowHash);
+
+    struct UserInfo {
+        address user;
+        address nftContract;
+        int tokenID;
+        int subnet;
     }
 
-    enum State {
-        INITIATED,
-        READY_ONE,
-        READY_SWAP,
-        COMPLETE
+    struct EscrowTX {
+        UserInfo orderOne;
+        UserInfo orderTwo;
+        bool isReady;
     }
 
-    struct Escrow {
-        address alice;
-        address bob;
-        address aliceNFT;
-        address bobNFT;
-        string aliceSubnet;
-        string bobSubnet;
-        State currentState;
-        Ready_One readyOne;
+    // Address of relayer
+    address private relayer;
+
+    // Maps Escrow TX hashes to actual Escrow TX info
+    mapping(bytes32 => EscrowTX) private escrowDirectory;
+
+    // When initialized, contract sets argument as address of relayer
+    constructor(address _relayer) {
+        relayer = _relayer;
     }
 
-    uint256 public nextEscrow;
+    // Function that creates an escrowTX
+    function createEscrowTX(address _nftContract1, int _tokenID1, int _subnetID1, address _user2, address _nftContract2, int _tokenID2, int _subnet2) public {
+        UserInfo memory userInfo1 = UserInfo(msg.sender, _nftContract1, _tokenID1, _subnetID1);
+        UserInfo memory userInfo2 = UserInfo(_user2, _nftContract2, _tokenID2, _subnet2);
 
-    mapping(uint256 => Escrow) public escrows;
+        EscrowTX memory newEscrow = EscrowTX(userInfo1, userInfo2, false);
+        bytes32 hash = keccak256(abi.encode(newEscrow));
 
-    address relayer;
+        require(escrowDirectory[hash].orderOne.user != address(0), "EscrowTX already exists!"); 
 
-    modifier _isRelayer(address _relayer) {
-        require(_relayer == relayer, "Not Relayer!");
-        _;
+        escrowDirectory[hash] = newEscrow;
+        emit NewEscrow(hash);
     }
 
-    function initiate(
-        address _alice,
-        address _bob,
-        address _aliceNFT,
-        address _bobNFT,
-        string memory _alicesubnet,
-        string memory _bobSubnet
-    ) external _isRelayer(msg.sender) {
-        escrows[nextEscrow] = Escrow(
-            _alice,
-            _bob,
-            _aliceNFT,
-            _bobNFT,
-            _alicesubnet,
-            _bobSubnet,
-            State.INITIATED,
-            Ready_One.NULL
-        );
-        nextEscrow++;
-    }
-
-    function ready(address _person, uint256 _escrowID)
-        external
-        _isRelayer(msg.sender)
-    {
-        Escrow storage escrow = escrows[_escrowID];
-        if (escrow.currentState == State.INITIATED) {
-            escrow.currentState = State.READY_ONE;
-            if (_person == escrow.alice) {
-                escrow.readyOne = Ready_One.ALICE;
-            } else if (_person == escrow.bob) {
-                escrow.readyOne = Ready_One.BOB;
-            }
-        } else if (escrow.currentState == State.READY_ONE) {
-            escrow.currentState = State.READY_SWAP;
-        }
-    }
-
-    function swapDone(uint256 _escrowID) external _isRelayer(msg.sender) {
-        Escrow storage escrow = escrows[_escrowID];
-        require(escrow.currentState == State.READY_SWAP);
-        escrow.currentState = State.COMPLETE;
-    }
 }
--- contracts/franchise.sol
+++ contracts/franchise.sol
@@ -1,55 +1,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+// //import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 
-contract FranchiseEscrow {
-    struct Halfrow {
-        address alice;
-        address bob;
-        address nft;
-        uint256 tokenId;
-        State currentState;
-    }
+// contract FranchiseEscrow {
+//     struct Halfrow {
+//         address alice;
+//         address bob;
+//         address nft;
+//         uint256 tokenId;
+//         State currentState;
+//     }
 
-    //add in fail state
-    enum State {
-        INITIATED,
-        RECIEVED,
-        RELEASED
-    }
+//     //add in fail state
+//     enum State {
+//         INITIATED,
+//         RECIEVED,
+//         RELEASED
+//     }
 
-    uint256 public nextHalfrow;
+//     uint256 public nextHalfrow;
 
-    mapping(uint256 => Halfrow) public halfrows;
+//     mapping(uint256 => Halfrow) public halfrows;
 
-    address relayer;
+//     address relayer;
 
-    modifier _isRelayer(address _relayer) {
-        require(_relayer == relayer, "Not Relayer!");
-        _;
-    }
+//     modifier _isRelayer(address _relayer) {
+//         require(_relayer == relayer, "Not Relayer!");
+//         _;
+//     }
 
-    function initiate(
-        address _alice,
-        address _bob,
-        address _nft,
-        address _tokenId
-    ) external _isRelayer(msg.sender) {
-        halfrows[nextHalfrow] = Halfrow(
-            _alice,
-            _bob,
-            _nft,
-            _tokenId,
-            State.INITIATED
-        );
-        nextHalfrow++;
-    }
+//     function initiate(
+//         address _alice,
+//         address _bob,
+//         address _nft,
+//         address _tokenId
+//     ) external _isRelayer(msg.sender) {
+//         halfrows[nextHalfrow] = Halfrow(
+//             _alice,
+//             _bob,
+//             _nft,
+//             _tokenId,
+//             State.INITIATED
+//         );
+//         nextHalfrow++;
+//     }
 
-    function recieve(uint256 _halfrowID) external {
-        Halfrow storage halfrow = halfrows[_halfrowID];
-        // Get nft address
-        INFT interface_nft = INFT(halfrow.nft);
-        interface_nft.safe_tranfer_from(halfrow.alice, this, halfrow.tokenId);
-    }
-}
+//     function recieve(uint256 _halfrowID) external {
+//         Halfrow storage halfrow = halfrows[_halfrowID];
+//         // Get nft address
+//         INFT interface_nft = INFT(halfrow.nft);
+//         interface_nft.safe_tranfer_from(halfrow.alice, this, halfrow.tokenId);
+//     }
+// }
