d04e470d438932efec9bc3a54a2b8ced7212dd35
Rodrigo Villar <rodrigojvillarg@gmail.com>
2022-04-30 10:05:51 -0400

    add vault.sol

--- contracts/escrow.sol
+++ contracts/escrow.sol
@@ -89,9 +89,7 @@ Smart contract to be stored on NFTEScrow Subnet
 */
 contract Escrow {
 
-    // Event to be emitted when new escrow tx is initiated
-    event NewEscrow(bytes32 indexed escrowHash);
-
+    // Struct representing user information on subnet
     struct UserInfo {
         address user;
         address nftContract;
@@ -99,10 +97,12 @@ contract Escrow {
         int subnet;
     }
 
+    // Struct 
     struct EscrowTX {
         UserInfo orderOne;
         UserInfo orderTwo;
-        bool isReady;
+        bool isReady1;
+        bool isReady2;
     }
 
     // Address of relayer
@@ -110,6 +110,12 @@ contract Escrow {
 
     // Maps Escrow TX hashes to actual Escrow TX info
     mapping(bytes32 => EscrowTX) private escrowDirectory;
+
+    // Event to be emitted when new escrow tx is initiated
+    event NewEscrow(bytes32 indexed escrowHash);
+
+    // Event to be emitted when both NFTs are locked up
+    event ReleaseEscrow(bytes32 indexed escrowHash);
 
     // When initialized, contract sets argument as address of relayer
     constructor(address _relayer) {
@@ -121,13 +127,31 @@ contract Escrow {
         UserInfo memory userInfo1 = UserInfo(msg.sender, _nftContract1, _tokenID1, _subnetID1);
         UserInfo memory userInfo2 = UserInfo(_user2, _nftContract2, _tokenID2, _subnet2);
 
-        EscrowTX memory newEscrow = EscrowTX(userInfo1, userInfo2, false);
-        bytes32 hash = keccak256(abi.encode(newEscrow));
+        EscrowTX memory newEscrow = EscrowTX(userInfo1, userInfo2, false, false);
+        bytes32 hash = keccak256(abi.encode(newEscrow), block.number);
 
-        require(escrowDirectory[hash].orderOne.user != address(0), "EscrowTX already exists!"); 
+        require(escrowDirectory[hash].orderOne.user == address(0), "EscrowTX already exists!"); 
 
         escrowDirectory[hash] = newEscrow;
         emit NewEscrow(hash);
     }
+
+    function nftLocked(bytes32 _hash, address _user) public {
+        EscrowTX memory localEscrow = escrowDirectory[_hash];
+        require(localEscrow.orderOne.user == _user || localEscrow.orderTwo.user == _user, "Address not involved in Escrow TX!");
+        if (localEscrow.orderOne.user == _user) {
+            require(localEscrow.isReady1 == false, "NFT is already locked up!");
+            escrowDirectory[_hash].isReady1 = true;
+        } else {
+            require(localEscrow.isReady2 == false, "NFT is already locked up!");
+            escrowDirectory[_hash].isReady2 = true;
+        }
+
+        if (escrowDirectory[_hash].isReady1 && escrowDirectory[_hash].isReady2) {
+            emit ReleaseEscrow(_hash);
+            delete escrowDirectory[_hash];
+        }
+    }
+
 
 }
