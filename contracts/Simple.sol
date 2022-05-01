80ccfdbc57d52f63590e3672c2ac21b1b931e24b
Jackson Prybil <jacksonprybil@gmail.com>
2022-05-01 10:31:09 -0400

    simple contract

---
+++ contracts/Simple.sol
@@ -0,0 +1,13 @@
+/*
+Contract to test deployment for subnets
+*/
+
+pragma solidity ^0.8.0;
+
+contract Simple {
+
+    function test() public pure returns(string memory) {
+        return "Hello World!";
+    }
+
+}
