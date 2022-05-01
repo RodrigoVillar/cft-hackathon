bae4c597f04983a1a5f2ce4d259c8ace5a30945a
Zachary Gelfond <zgelfond@gmail.com>
2022-05-01 07:17:31 -0400

    added web.js to listen for event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,11 @@ import { OnboardingButton } from './components/Onboarding';
 import ContractArtifact from './contracts/Escrow.json'
 import contractAddress from './contracts/escrow-address.json'
 
+import Web3 from 'web3';
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");//something rpc endpoint
+const ABI = ''; //todo
+const CONTRACT_ADDRESS = ''; //todo
+const escrowContract = '';//new Web3.Contract(ABI, CONTRACT_ADDRESS);
 
 class App extends React.Component {
   constructor() {
@@ -17,7 +22,8 @@ class App extends React.Component {
     this.state = {
       isConnected: false,
       contract: null,
-      isInitiated: false
+      isInitiated: false,
+      hashEvent: null,
     }
 
     this.onConnected = this.onConnected.bind(this)
@@ -40,6 +46,7 @@ class App extends React.Component {
       isConnected: true,
       contract,
       isInitiated: false,
+      hashEvent: null,
     })
 
 
@@ -48,11 +55,45 @@ class App extends React.Component {
   async initiate() {
     console.log('initiating contract')
     await this.state.contract.createEscrowTX()//TODO
+    //this.state.contract.once('NewEscrow', initiate());
+  }
+
+  async catchEvent() {
+    let options = {
+      filter: {
+        value: [],
+      },
+      fromBlock: 0
+    };
+
+    escrowContract.events.NewEscrow(options, foo);
+    function foo(err, contractEvent) {
+      if (err) {
+        console.error('idk', err);
+        return;
+      }
+      const {
+        event,
+        returnValues,
+        blockNumber,
+      } = contractEvent;
+      const { hash0 } = returnValues;
+      this.state.hashEvent = hash0;
+    }
 
   }
 
+
+
   render() {
 
+    const HashComponent = <div>
+      {this.state.hashEvent
+        ? <p>New escrow hash: <br />&ldquo;{this.state.hashEvent}&rdquo;</p>
+        : <p>No new escrow yet...</p>
+      }
+    </div>
+    //add fallback, install things. google thing
     return (
       <div className="App">
         <h1>Cross-Subnet NFT Escrow</h1>
@@ -66,6 +107,8 @@ class App extends React.Component {
             <button onClick={this.initiate}>Initiate Escrow</button>
           </div>
         }
+        {this.state.isConnected && HashComponent}
+
       </div>
     )
   }
